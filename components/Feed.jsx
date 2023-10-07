2911aa40094e3366fa1678532a57b83a896bb9a3
Esan Samuel <esansamuel555@gmail.com>
2023-10-07 23:44:55 +0100

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -9,7 +9,7 @@ const CardList = ({ data, handleTagClick }) => {
     <div className="mt-16 flex justify-center flex-col items-center gap-3">
       {data?.length > 0 ? (
         <>
-          {data.slice().reverse().map((post) => (
+          {data.reverse().map((post) => (
             <PromptCard
               key={post._id}
               post={post}
